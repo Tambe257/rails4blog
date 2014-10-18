42a40f6f6eb76dffa7d0d609b229ba3f4b44978b
Tony Tambe <tambe257@gmail.com>
2014-10-18 10:01:33 -0400

    Added devise

---
+++ app/models/post.rb
@@ -0,0 +1,13 @@
+class Post < ActiveRecord::Base
+
+  belongs_to :user
+  
+  def self.search(search)
+    if search
+      losearch = search.downcase
+      find(:all, :conditions => ['lower(title) LIKE ? OR lower(body) LIKE ?', "%#{search.downcase}%", "%#{search.downcase}%"], order: "created_at desc")
+    else
+      find(:all, order: "created_at desc")
+    end
+  end 
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4blog::Application.config.secret_key_base = 'f98b64f569dcfedaab516e2d355741ca5ea6b3650ee958553a066f1e08dae9a6a0ffb7ec5c5fec87d952ee3f9d000cc868582d2c5966ee603dd3f555880a7160'
