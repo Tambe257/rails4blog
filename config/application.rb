4a1d8c1a45bf85843def96445d4059b006e13b2f
Tony Tambe <tambe257@gmail.com>
2014-10-26 10:05:57 -0400

    config.active_support.escape_html_entities_in_json = true

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module Rails4blog
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.active_support.escape_html_entities_in_json = true
+
     config.assets.initialize_on_precompile = false
 
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
